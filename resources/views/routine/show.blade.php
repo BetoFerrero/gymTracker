a57203e3962c7e30e96d8982844fd96901112c00
Alberto Ferrero <albertoferrerosanchez@gmail.com>
2023-06-12 19:09:28 +0200

    routine_show

--- resources/views/routine/show.blade.php
+++ resources/views/routine/show.blade.php
@@ -10,7 +10,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg dark:bg-gray-900">
                 <livewire:routine-detail :routine="$routine">
-                <p>UUID: {{ Uuid::uuid4()->toString() }}</p>
             </div>
         </div>
     </div>
