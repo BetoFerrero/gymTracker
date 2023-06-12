b09db2a80aa2e96422947e092a523ef571f70966
Alberto Ferrero <albertoferrerosanchez@gmail.com>
2023-06-12 22:45:59 +0200

    vite https config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: { https: true },
     plugins: [
         laravel({
             input: [
